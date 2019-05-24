227fdcf919e0cbac8df5b5a39247d2f6b49d748b
subinoyroy <subinoyroy@gmail.com>
2019-05-24 23:16:47 +0700

    MethodNotations added

--- src/exercises/Ex1.scala
+++ src/exercises/Ex1.scala
@@ -1,13 +1,36 @@
 package exercises
 
+abstract class MyList {
+  def head:Int
+  def tail: MyList
+  def isEmpty: Boolean
+  def add(data:Int): MyList
+}
+
+object Empty extends MyList{
+  override def head: Int = throw new NoSuchElementException
+
+  override def tail: MyList = throw new NoSuchElementException
+
+  override def isEmpty: Boolean = true
+
+  override def add(data: Int): MyList = new Cons(data, Empty)
+}
+
+class Cons(h:Int, t:MyList) extends MyList {
+  override def head: Int = h
+
+  override def tail: MyList = t
+
+  override def isEmpty: Boolean = false
+
+  override def add(data: Int): MyList = new Cons(data, this)
+}
+
 object Ex1 extends App{
-  abstract class MyList {
-    val head:Node
-    class Node(val data:Int) {
-      val next:Node = new Node(data)
-    }
-  }
 
-
-
+  val list:Cons = new Cons(1,new Cons(2,new Cons(3,Empty)))
+  System.out.println(list.head)
+  System.out.println(list.tail.head)
+  System.out.println(list.tail.tail.head)
 }
